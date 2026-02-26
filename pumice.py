a23099e08a1ebd99aa479831a4cc1c57c3056165
Emily Prieto <61399347+stygian-ash@users.noreply.github.com>
2026-02-26 08:38:19 -0500

    feat: support Obsidian syntax for vault-relative images
    Resolve the absolute paths to images using the `![](image.png)` syntax,
    where the image URL is not an absolute or relative path, but instead
    refers to any file called `image.png` in the Obsidian vault.

--- pumice.py
+++ pumice.py
@@ -4,7 +4,7 @@ import re
 import sys
 import json
 import itertools
-from typing import Any
+from typing import Any, Callable
 from pathlib import Path
 from io import TextIOWrapper
 from argparse import ArgumentParser, ArgumentError
@@ -79,6 +79,8 @@ def extract_document_from_pandoc_cmdline(cmdline: list[str]) -> str | None:
     return args.inputfile
 
 
+@require(lambda process: process is None or process.cmdline[0].endswith("pandoc"))
+@ensure(lambda result: result is None or result.exists())
 def get_pandoc_document(process: Process | None = None) -> Path | None:
     """Find the input file that a Pandoc process is operating on.
 
@@ -99,12 +101,22 @@ def get_pandoc_document(process: Process | None = None) -> Path | None:
     return Path(document)
 
 
+@require(lambda path: path.exists())
 def is_vault_root(path: Path) -> bool:
     """Determine whether a directory path refers to the root of an Obsidian
     vault."""
     return (path / ".obsidian").is_dir()
 
 
+def path_ends_with(path: Path, suffix: Path) -> bool:
+    """Determine whether a path ends with another path."""
+    return str(path).endswith(str(suffix))
+
+
+@require(lambda path: path.exists())
+@ensure(
+    lambda result, filename: result is None or path_ends_with(result, Path(filename))
+)
 def find_note_uncle(path: Path, filename: str) -> Path | None:
     """Search for a file in a directory and all of its ancestors up to the
         Obsidian vault root.
@@ -132,6 +144,60 @@ def fix_equation_environments(key: str, value, format: str, meta):
         return RawInline("tex", value[1])
 
 
+@require(lambda include: not include.startswith("/"), "must be a relative path")
+@require(lambda include: len(Path(include).parts) > 0)
+@require(lambda working_dir: working_dir.is_absolute())
+@ensure(
+    lambda result, include: result is None
+    or (result.exists() and str(result).endswith(str(Path(include))))
+)
+def resolve_vault_relative_include(include: str, working_dir: Path) -> Path | None:
+    vault_root = None
+    for parent in itertools.chain([working_dir], working_dir.parents):
+        if (path := parent / include).exists():
+            return path
+        vault_root = parent
+        if is_vault_root(parent):
+            break
+    assert (
+        vault_root != None
+    ), "it should be impossible for no path to be assigned to vault_root"
+    part = Path(include)
+    for root, dirnames, filenames in vault_root.walk():
+        if len(part.parts) > 1:
+            if part.parts[0] in dirnames and (ret := root / part).exists():
+                return ret
+        else:
+            assert len(part.parts) > 0
+            if part.parts[-1] in filenames:
+                ret = root / part
+                assert ret.exists()
+                return ret
+    logger.warning(f'Failed to resolve vault-relative path "{include}"')
+    return None
+
+
+@require(lambda working_dir: working_dir.is_absolute())
+def vault_relative_path_resolver(
+    working_dir: Path,
+) -> Callable[[str, Any, str, Any], Any]:
+    """Resolve relative paths to images included using the `[](image.png)` syntax."""
+
+    def resolver(key: str, value, format: str, meta):
+        if key == "Image":
+            [_, _, image] = value
+            if (
+                not (working_dir / image[0]).is_file()
+                and not Path(image[0]).is_absolute()
+            ):
+                logger.debug(f'Found unresolved local path "{image[0]}"')
+                if resolved := resolve_vault_relative_include(image[0], working_dir):
+                    logger.info(f'Resolving local path to "{str(resolved)}"')
+                    image[0] = str(resolved)
+
+    return resolver
+
+
 # TODO: if a preamble already exists, preserve it and absolutize it if it is relative
 @require(lambda preamble: preamble.suffix in {".sty", ""})
 def insert_preamble(ast: Any, preamble: Path) -> Any:
@@ -150,13 +216,15 @@ def filter_ast(ast: Any, format: str = "") -> Any:
     :return: A dictionary representation of the filtered AST."""
 
     logger.info(f"Filtering JSON for output format {format}")
+    filters: list[Any] = [fix_equation_environments]
     if document := get_pandoc_document():
+        document = document.absolute()
         logger.info(f'Input document is "{str(document)}"')
         # Preamble from the Extended MathJax plugin
         if preamble := find_note_uncle(document, "preamble.sty"):
             ast = insert_preamble(ast, preamble)
+        filters.append(vault_relative_path_resolver(document.parent))
 
-    filters = [fix_equation_environments]
     logger.info(f"Applying {len(filters)} AST transformations")
     ast = json.loads(applyJSONFilters(filters, json.dumps(ast), format))
     return ast
